d59b167edacf629024dada8caf20f425abeb2a8f
reuk <reuben@null.local>
2014-05-19 10:20:35 +0200

    maybe I'll work out how to fix this today

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,6 +1,6 @@
-# cabal install --ghc-options="-rtsopts -fforce-recomp -Wall -prof -auto-all" -fdebug --enable-executable-profiling
-cabal install --ghc-options="-rtsopts -Wall -prof -auto-all" -fdebug --enable-executable-profiling
-# tracer ~/Desktop/trace.json +RTS -h -p -sstderr
-flattener ~/Desktop/trace.json ~/Desktop/out.wav +RTS -h -p -sstderr
+cabal install --ghc-options="-rtsopts -fforce-recomp -Wall -prof -auto-all" -fdebug --enable-executable-profiling
+# cabal install --ghc-options="-rtsopts -Wall -prof -auto-all" -fdebug --enable-executable-profiling 
+# tracer ~/Desktop/trace2.json +RTS -h -p -sstderr
+flattener ~/Desktop/trace2.json ~/Desktop/out.wav +RTS -h -p -sstderr
 hp2ps -c tracer.hp
 hp2ps -c flattener.hp
--- flattener/Flattener.hs
+++ flattener/Flattener.hs
@@ -11,7 +11,7 @@ import Impulse
 import Data.WAVE
 import Control.Applicative
 import System.Environment
-import Data.List (transpose, foldl1')
+import Data.List (transpose)
 import Control.DeepSeq
 import Data.Aeson hiding (decode)
 import Data.ByteString.Lazy (readFile)
@@ -22,17 +22,18 @@ import System.IO hiding (readFile)
 import Pipes.Aeson hiding (decoded, decode)
 import Pipes.Aeson.Unchecked
 import Control.Monad.State.Strict
-import Lens.Family.State.Strict
+import Lens.Family
+import Lens.Family.State.Strict 
 import Data.Array.IO
+import Debug.Trace
+import Pipes.Prelude (fold)
+import Control.Foldl (purely)
 
 spk :: [Speaker]
 spk = [Speaker (Vec3 0 1 0) 0.5, Speaker (Vec3 1 0 0) 0.5]
 
-sampleRate :: Flt
-sampleRate = 44100.0
-
-flattener :: [Speaker] -> String -> String -> IO ()
-flattener speakers raytraceFile outFile = do
+flattener :: [Speaker] -> Flt -> Int -> String -> String -> IO ()
+flattener speakers sampleRate bitDepth raytraceFile outFile = do
     input <- readFile raytraceFile
     let x = decode' input :: Maybe [RayTrace]
     case x of
@@ -51,7 +52,14 @@ main :: IO ()
 --         _ -> putStrLn "program takes two arguments"
 
 rayTraceLength (RayTrace _ impulses) = maximum $ map time impulses
-    where   maximum = foldl1' max
+
+foldAll' step begin done = go begin
+  where
+    go x = do
+        ea <- draw
+        case ea of
+            Nothing -> return (done x)
+            Just a  -> go $! step x a
 
 -- remember always the beauty of this function
 -- actually don't because it's not strict in the argument to 'max' so
@@ -59,8 +67,19 @@ rayTraceLength (RayTrace _ impulses) = maximum $ map time impulses
 lastSampleTimeForFile :: String -> IO Flt
 lastSampleTimeForFile f = withFile f ReadMode $ \hIn -> do
     evalStateT parseLastSampleTime (P.fromHandle hIn)
+    where   parseLastSampleTime = zoom decoded (foldAll' maxSample 0 id)
 
-parseLastSampleTime = zoom decoded (foldAll (flip $ max . rayTraceLength) 0 id)
+maxSample = flip $ max . rayTraceLength
+
+-- blah = withFile "/Users/reuben/Desktop/trace.json" ReadMode $ \hIn ->
+--     decoded ^. (P.fromHandle hIn)
+-- 
+-- blah' = blah >> return ()
+
+-- main' :: IO ()
+-- main' = do
+--     last <- purely fold $ maxSample 0 id blah
+--     print $ show last
 
 channelForFile f l sr speaker = withFile f ReadMode $ \hIn -> do
     t <- newArray (0, l) (pure 0)
@@ -88,18 +107,25 @@ createAndProcessChannelForFile f samples sr speaker = do
 createAllChannelsForFile :: String -> Flt -> [Speaker] -> IO [[Flt]]
 createAllChannelsForFile f sr speakers = do
     lastTime <- lastSampleTimeForFile f
-    let lastSample = secondsToSamples sampleRate lastTime
+    let lastSample = secondsToSamples sr lastTime
     a <- mapM (createAndProcessChannelForFile f lastSample sr) speakers
     normalize a
     mapM getElems a
 
+flatten :: String -> [Speaker] -> Flt -> Int -> String -> IO ()
+flatten inFile speakers sampleRate bitDepth outFile = do
+    channels <- createAllChannelsForFile inFile sampleRate speakers
+    let out = force $ (map (map doubleToSample) $!! (transpose channels))
+    putWAVEFile outFile (WAVE waveheader out)
+    where   waveheader = WAVEHeader (length speakers) (round sampleRate) bitDepth Nothing
+
 main = do
     args <- getArgs
     case length args of
+--         2 -> 
+--             flatten (args !! 0) spk 44100 16 (args !! 1)
         2 -> do
-            channels <- createAllChannelsForFile (args !! 0) sampleRate spk
-            let out = force $ (map (map doubleToSample) $!! (transpose channels))
-            putWAVEFile (args !! 1) (WAVE waveheader out)
-            where   waveheader = WAVEHeader (length spk) (round sampleRate) 16 Nothing
+            last <- lastSampleTimeForFile (args !! 0)
+            print $ show last
         _ -> putStrLn "program takes two arguments"
 
